ca4eecdb6a2b54d13d5de3429dbb370cca133d04
Piper Thunstrom <pathunstrom@gmail.com>
2020-04-10 20:42:44 -0400

    Adds basic state to Player.

--- survival/player.py
+++ survival/player.py
@@ -16,19 +16,53 @@ class Player(Sprite):
     image = Square(200, 55, 40)
     target_facing = None
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.state = Neutral(self, None)
+
     def on_button_released(self, event: events.ButtonReleased, signal):
+        self.state.on_button_released(event, signal)
+
+    def on_mouse_motion(self, event: events.MouseMotion, signal):
+        self.state.on_mouse_motion(event, signal)
+
+    def on_update(self, event: events.Update, signal):
+        self.state.on_update(event, signal)
+
+
+class State:
+
+    def __init__(self, parent, last_state):
+        self.parent = parent
+        self.last_state = last_state
+
+    def on_button_released(self, event, signal):
+        pass
+
+    def on_mouse_motion(self, event, signal):
+        pass
+
+    def on_update(self, event, signal):
+        pass
+
+
+class Neutral(State):
+
+    def on_button_released(self, event, signal):
         if event.button == buttons.Primary:
             event.scene.add(
                 Hitbox(
-                    position=self.position + self.facing * 2
+                    position=self.parent.position + self.parent.facing * 2
                 )
             )
 
-    def on_mouse_motion(self, event: events.MouseMotion, signal):
-        self.target_facing = (event.position - self.position).normalize()
+    def on_mouse_motion(self, event, signal):
+        self.parent.target_facing = (
+                event.position - self.parent.position
+        ).normalize()
 
-    def on_update(self, event: events.Update, signal):
-        if self.target_facing is not None:
-            self.facing = calculate_rotation(
-                self.facing, self.target_facing
+    def on_update(self, event, signal):
+        if self.parent.target_facing is not None:
+            self.parent.facing = calculate_rotation(
+                self.parent.facing, self.parent.target_facing
             ).normalize()
