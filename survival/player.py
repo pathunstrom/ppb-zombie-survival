f4059c38f297a9c42fae4156e1951363471a9a3f
Piper Thunstrom <pathunstrom@gmail.com>
2020-04-10 20:15:30 -0400

    Adds Player

---
+++ survival/player.py
@@ -0,0 +1,6 @@
+from ppb import Sprite
+from ppb.assets import Square
+
+
+class Player(Sprite):
+    image = Square(200, 55, 40)
--- survival/sandbox.py
+++ survival/sandbox.py
@@ -1,5 +1,11 @@
 from ppb import BaseScene
 
+from survival.player import Player
+
 
 class Sandbox(BaseScene):
     background_color = 0, 0, 0
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add(Player())
