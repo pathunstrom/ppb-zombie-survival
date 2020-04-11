f94741185328164a9a6be612739c46cc500f98f5
Piper Thunstrom <pathunstrom@gmail.com>
2020-04-10 20:19:40 -0400

    Adds basic attack wiring.

--- survival/player.py
+++ survival/player.py
@@ -1,3 +1,4 @@
+from ppb import buttons
 from ppb import events
 from ppb import Sprite
 from ppb.assets import Square
@@ -7,9 +8,21 @@ from survival.utils import asymptotic_average_builder
 calculate_rotation = asymptotic_average_builder(12)
 
 
+class Hitbox(Sprite):
+    pass
+
+
 class Player(Sprite):
     image = Square(200, 55, 40)
     target_facing = None
+
+    def on_button_released(self, event: events.ButtonReleased, signal):
+        if event.button == buttons.Primary:
+            event.scene.add(
+                Hitbox(
+                    position=self.position + self.facing * 2
+                )
+            )
 
     def on_mouse_motion(self, event: events.MouseMotion, signal):
         self.target_facing = (event.position - self.position).normalize()
