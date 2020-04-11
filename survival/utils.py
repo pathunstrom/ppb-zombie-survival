637d555f776245bf1ae15a7a9c5910e5e6685467
Piper Thunstrom <pathunstrom@gmail.com>
2020-04-10 20:16:16 -0400

    Adds asymptotic_average_builder

---
+++ survival/utils.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+
+def asymptotic_average_builder(new_part: Union[float, int]):
+    """
+    Given a percentage as a float, return an interpolation function based on it.
+
+    Example:
+       ``interpolate_builder(10)``
+
+    The returned function will use .1 of the new value and .9 of the old value
+    to calculate the current value.
+
+    :param new_part: float
+    :return: Callable[[Any, Any], Any]
+    """
+    new = new_part / 100.0
+    old = 1 - new
+
+    def interpolate(old_input, new_input):
+        return old_input * old + new_input * new
+
+    return interpolate
