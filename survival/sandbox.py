2c394807509671d85f245c7a1b15a5e267d3fc02
Piper Thunstrom <pathunstrom@gmail.com>
2020-04-10 21:12:42 -0400

    Adds follow cam functionality.

--- survival/sandbox.py
+++ survival/sandbox.py
@@ -1,6 +1,10 @@
 from ppb import BaseScene
 
 from survival.player import Player
+from survival.utils import asymptotic_average_builder
+
+
+calc_cam_pos = asymptotic_average_builder(5)
 
 
 class Sandbox(BaseScene):
@@ -9,3 +13,9 @@ class Sandbox(BaseScene):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.add(Player())
+
+    def on_pre_render(self, event, signal):
+        player = next(self.get(kind=Player))
+        camera = self.main_camera
+
+        camera.position = calc_cam_pos(camera.position, player.position)
